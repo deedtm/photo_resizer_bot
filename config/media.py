717fd2321da6fe71da115341d4df906a6ccac5af
deedtm <misha.nikto000@gmail.com>
2025-02-27 01:59:52 +0400

    fix for filenotfounderror

--- config/media.py
+++ config/media.py
@@ -4,3 +4,6 @@ from . import _config
 
 INPUT_PATH = os.sep.join(_config.get('media', 'input_path_dir').split('.'))
 OUTPUT_PATH = os.sep.join(_config.get('media', 'output_path_dir').split('.'))
+
+os.makedirs(INPUT_PATH, exist_ok=True)
+os.makedirs(OUTPUT_PATH, exist_ok=True)
